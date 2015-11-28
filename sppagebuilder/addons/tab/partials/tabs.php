16eaf093fadffa73b98db590e26b2f5a8037fdc1
Sebastian Antosch <s.antosch@i-san.de>
2015-11-28 11:48:08 +0100

    [REFACTOR] tabs use template [FIX] tabs

---
+++ sppagebuilder/addons/tab/partials/tabs.php
@@ -0,0 +1,28 @@
+<div class="sppb-addon sppb-addon-tab <?= $class ?>">
+    <?php if($title): ?>
+        <<?= $heading_selector ?> class="sppb-addon-title"><?= $title ?></<?= $heading_selector ?>>
+    <?php endif; ?>
+
+    <div class="sppb-addon-content sppb-tab">
+
+        <ul class="nav nav-<?= $style ?>">
+            <?php foreach($sppbTabArray as $key=>$tab): ?>
+                <li class="<?= ($key==0) ? "active" : "" ?>">
+                    <a role="tab" data-toggle="tab" href="#tab-<?= $tagNumber ?>-<?= $key ?>">
+                        <?= $tab['icon']?> <?= $tab['title']?>
+                    </a>
+                </li>
+            <?php endforeach; ?>
+        </ul>
+
+        <div class="tab-content">
+            <?php foreach($sppbTabArray as $key=>$tab): ?>
+                <div class="tab-pane fade <?= ($key==0) ? "active in" : "" ?>" id="tab-<?= $tagNumber ?>-<?= $key ?>">
+                    <?= $tab['content'] ?>
+                </div>
+            <?php endforeach; ?>
+        </div>
+
+    </div>
+</div>
+
