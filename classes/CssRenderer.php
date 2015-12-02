51a1952956ff3417429be4bb93be0647b1126cb8
Sebastian Antosch <s.antosch@i-san.de>
2015-12-02 20:38:14 +0100

    [COMPATIBILITY] provide getallheaders() alternative for non-apache or older CLI-PHP

--- classes/CssRenderer.php
+++ classes/CssRenderer.php
@@ -46,7 +46,28 @@ class CssRenderer {
             }
         });
 
+        /*
+         * Crossplattform getallheaders method
+         */
+        if (!function_exists('getallheaders'))
+        {
+            function getallheaders()
+            {
+                $headers = '';
+                foreach ($_SERVER as $name => $value)
+                {
+                    if (substr($name, 0, 5) == 'HTTP_')
+                    {
+                        $headers[str_replace(' ', '-', ucwords(strtolower(str_replace('_', ' ', substr($name, 5)))))] = $value;
+                    }
+                }
+                return $headers;
+            }
+        }
 
+        /*
+         * Render SCSS if necessary
+         */
         $headers = getallheaders();
         $forceRefresh = ($headers['Cache-Control'] == "no-cache" || $headers["Pragma"] == "no-cache" || isset($_GET['nocache']));
         if(!file_exists($css_path) || $forceRefresh)
